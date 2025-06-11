39bd393536a6380807c2a56f83c3ca55f129575c
SoundDrill31 <84176052+sounddrill31@users.noreply.github.com>
2025-06-11 18:33:45 +0530

    Add initial support for a touch button - Add State happy(headpats) - Add State Shook(too many movements)

--- main.py
+++ main.py
@@ -4,60 +4,65 @@
 from MPU6050 import MPU6050
 
 from os import listdir, chdir
-from machine import Pin
+from machine import TouchPad,Pin
 from time import sleep_ms
 
+# Movement Definitions
 mpu = MPU6050()
+move_val = 0 # Track shake value
+fragile = 2 # Number of shakes the buddy can handle
 
-# List all files directory.
-print("Root directory: {} \n".format(listdir()))
+# Touch Definitions
+capacitiveValue = 300
+touch_threshold = 150 # Touch threshold to be adjusted
+touch_pin = TouchPad(Pin(13))
+headpat_val = 0 # Track value for headpats
+headpat_threshold = 3
 
-# Change filename and fileformat here.
-filename = "{}%s.{}".format("data_logs", "txt")
+print("Starting Up! (˶ᵔ ᵕ ᵔ˶)")
 
-# Increment the filename number if the file already exists.
-i = 0
-while (filename % i) in listdir():
-    i += 1
-
-# Save file in path /
-with open(filename % i, "w") as f:
-    cols = ["Temp", "AcX", "AcY", "AcZ"]
-    f.write(",".join(cols) + "\n")
+while True:    
+    # Accelerometer Data
+    accel = mpu.read_accel_data() # read the accelerometer [ms^-2]
+    aX = accel["x"]
+    aY = accel["y"]
+    aZ = accel["z"]
+    print("x: " + str(aX) + " y: " + str(aY) + " z: " + str(aZ))
+  
+    # Gyroscope Data
+    gyro = mpu.read_gyro_data()   # read the gyro [deg/s]
+    gX = gyro["x"]
+    gY = gyro["y"]
+    gZ = gyro["z"]
+    print("x:" + str(gX) + " y:" + str(gY) + " z:" + str(gZ))
     
-    while True:
-        # Accelerometer Data
-        accel = mpu.read_accel_data() # read the accelerometer [ms^-2]
-        aX = accel["x"]
-        aY = accel["y"]
-        aZ = accel["z"]
-        print("x: " + str(aX) + " y: " + str(aY) + " z: " + str(aZ))
-    
-        # Gyroscope Data
-        # gyro = mpu.read_gyro_data()   # read the gyro [deg/s]
-        # gX = gyro["x"]
-        # gY = gyro["y"]
-        # gZ = gyro["z"]
-        # print("x:" + str(gX) + " y:" + str(gY) + " z:" + str(gZ))
-    
-        # Rough Temperature
-        temp = mpu.read_temperature()   # read the device temperature [degC]
-        # print("Temperature: " + str(temp) + "°C")
+    # Rough Temperature
+    temp = mpu.read_temperature()   # read the device temperature [degC]
+    print("Temperature: " + str(temp) + "°C")
 
-        # G-Force
-        # gforce = mpu.read_accel_abs(g=True) # read the absolute acceleration magnitude
-        # print("G-Force: " + str(gforce))
+    # G-Force
+    gforce = mpu.read_accel_abs(g=True) # read the absolute acceleration magnitude
+    mul_gforce = gforce * 1000 # make the tiny value more pronounced by multiplying with 1000
+    print("G-Force: " + str(gforce))
+    print("Multiplied G-Force:" + str(mul_gforce))
+    if mul_gforce >= 3000: # sudden frequent movement
+        move_val+=1
+    if mul_gforce <= 1500: # not moved in a while 
+        move_val=0
+    if move_val >= fragile: # check if we've exceeded the fragile threshold
+        print("I'm shook! I'm dizzy! (⸝⸝๑﹏๑⸝⸝)")
+        break
     
-        # Write to file
-        data = {"Temp" : temp,
-                "AcX" : aX,
-                "AcY" : aY,
-                "AcZ" : aZ
-                }
-        
-        push = [  str(data[k]) for k in cols ]
-        row = ",".join(push)
-        f.write(row + "\n")
-        
-        # Time Interval Delay in millisecond (ms)
-        sleep_ms(100)
+    # Touch Pins
+    capacitiveValue = touch_pin.read()
+    if capacitiveValue < touch_threshold:
+        print("Headpat detected (っ´ω`)ﾉ(˵•́ ᴗ •̀˵)")
+        headpat_val += 1
+    if headpat_val > headpat_threshold:
+        print("State Happy ( ˶ˆᗜˆ˵ )")
+        headpat_val = 0
+        continue
+
+    print("\n")
+    # Time Interval Delay in millisecond (ms)
+    sleep_ms(100)
